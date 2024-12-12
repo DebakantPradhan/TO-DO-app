62608fa4111d2f71ad32bf11fa87da24a560af28
DebakantPradhan <debakantpradhan646@gmail.com>
2024-12-12 19:52:52 +0530

    Implement task input and display in ToDo component; add styles and update app layout
    working fine!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToDoComponent } from './myComponents/to-do/to-do.component';
+import { Todo } from '../models/todo.model';
+
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToDoComponent],
+  imports: [ToDoComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TO-DO-app';
+
+  todoTitle :string = ""
+  todosList: Todo[] = [];
+  ngOnInit(){
+    this.todosList =[]
+    this.todoTitle = ""
+  }
+
+  addItem(){
+    if(this.todoTitle !== ""){
+      const newItem:Todo ={
+        slNo: 0,
+        title:this.todoTitle,
+        desc:"",
+        active:false
+      }
+      this.todosList.push(newItem)
+    }
+    this.todoTitle = ""
+  }
 }
--- src/app/myComponents/to-do/to-do.component.ts
+++ src/app/myComponents/to-do/to-do.component.ts
@@ -1,36 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Todo } from '../../../models/todo.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './to-do.component.html',
   styleUrls : ['./to-do.component.css']
 })
 export class ToDoComponent {
+  @Input() todos: Todo[] = [];
+  
 
-  todos: Todo[] = [];
+  
+
+
+
   constructor() { 
-    this.todos = [
-      {
-        slNo: 1,
-        title: 'Todo 1',
-        desc: 'Description 1',
-        active: true
-      },
-      {
-        slNo: 2,
-        title: 'Todo 2',
-        desc: 'Description 2',
-        active: true
-      },
-      {
-        slNo: 3,
-        title: 'Todo 3',
-        desc: 'Description 3',
-        active: false
-      }
-    ]
+    this.todos = []
   }
 }
